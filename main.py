1c974fead440c490f4c1319a17e08bc0f64f2d91
fissh <danialzmalik@gmail.com>
2025-12-06 12:09:14 -0500

    sorted list of dicts to get char count desc

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from stats import count_words, get_book_text, char_count
+from stats import count_words, get_book_text, char_count, sorted_list_of_dicts
 
 FRAKEN_PATH = "books/frankenstein.txt"
 
 
 def main():
+    print(f"{"="*10} BOOKBOT {"="*10}")
+
     frakenstein_txt = get_book_text(FRAKEN_PATH)
+
+    word_count = count_words(frakenstein_txt)
     char_dict = char_count(frakenstein_txt)
+
+    sorted_list = sorted_list_of_dicts(char_dict)
+    for dict in sorted_list:
+        c = dict["char"]
+        if c.isalpha():
+            print(f"{c}: {dict["num"]}")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 
 def get_book_text(filepath):
+    print(f"Analyzing book found at {filepath}")
     with open(filepath) as f:
         contents = f.read()
     return contents
@@ -15,6 +16,8 @@ def count_words(long_text: str):
     for word in split:
         x += 1
 
+    print(f"{'-'*12} Word Count {'-'*13}")
+    print(f"Found {len(split)} total words")
     return len(split)
 
 
@@ -33,4 +36,28 @@ def char_count(text_str):
     # for char in sorted_dict.items():
     #     print(f"'{char[0]}': {char[1]}")
 
+    print(f"{'-'*10} Character Count {'-'*10}")
     return sorted_dict
+
+
+def sorted_list_of_dicts(input_dict: dict):
+    """
+    create list of dicts sorted by num [(char: , num: )].
+    input is dict.
+    output will be list of dicts [(char: , num: )].
+    """
+
+    # step 1: splice into char and num dict
+    # step 2: create list of dicts
+    list_of_dicts = []
+    for key in input_dict.keys():
+        char_dict = {}
+
+        char_dict["char"] = key
+        char_dict["num"] = input_dict[key]
+        list_of_dicts.append(char_dict)
+
+    # step 3: sort list by values
+    sorted_list = sorted(list_of_dicts, key=lambda d: d["num"], reverse=True)
+
+    return sorted_list
