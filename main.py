14b3c8ae9404184f757c33a3432a637e2a835bd9
fissh <danialzmalik@gmail.com>
2025-12-06 12:17:04 -0500

    a add book argument and sys functionality

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from stats import count_words, get_book_text, char_count, sorted_list_of_dicts
-
-FRAKEN_PATH = "books/frankenstein.txt"
+import sys
 
 
 def main():
     print(f"{"="*10} BOOKBOT {"="*10}")
 
-    frakenstein_txt = get_book_text(FRAKEN_PATH)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    frakenstein_txt = get_book_text(sys.argv[1])
 
     word_count = count_words(frakenstein_txt)
     char_dict = char_count(frakenstein_txt)
